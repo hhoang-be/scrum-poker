18bdfc905032256d092f75a1e30b356088c89c7d
Hieu Hoang <hoangtrunghieu@gmail.com>
2020-06-23 23:03:11 +0200

    Cosmetic issue

--- app/src/main/java/com/hhoang/scrumpoker/CardScoreFragment.kt
+++ app/src/main/java/com/hhoang/scrumpoker/CardScoreFragment.kt
@@ -7,7 +7,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TableLayout
 import android.widget.TextView
 import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
